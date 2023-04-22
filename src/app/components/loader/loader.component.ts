fbea66850d1ac11e8d112a99a47b8b04a130fb2b
Mustafa-Seif <mustafaabdella64@gmail.com>
2023-04-22 16:29:54 +0200

    add loader on load

--- src/app/components/loader/loader.component.ts
+++ src/app/components/loader/loader.component.ts
@@ -17,32 +17,16 @@ export class LoaderComponent {
   IsLoader: boolean = false;
   block: boolean = false;
   constructor(private router: Router) {
+    this.IsLoader = true;
+      this.block = true
+    setTimeout(() => {
+      this.IsLoader = false;
+      this.block = false;
+      // setTimeout(() => (this.block = false), 300);
+    }, 500);
     this.router.events.subscribe((event: Event) => {
       if (event instanceof NavigationStart) {
-        this.IsLoader = true;
-        this.block = true;
-        // scroll to top 
         window.scrollTo(0, 0)
-      }
-      if (event instanceof NavigationEnd) {
-        // Hide progress spinner 
-        setTimeout(() => {
-          this.IsLoader = false;
-          this.block = false
-          // setTimeout(() => (this.block = false), 300);
-        }, 500);
-      }
-      if (event instanceof NavigationCancel) {
-        setTimeout(()=>{                           
-          this.IsLoader = true;
-          this.block = true;
-      }, 500);
-      }
-      if (event instanceof NavigationError) {
-        setTimeout(()=>{                           
-          this.IsLoader = true;
-          this.block = true;
-        }, 500);
       }
     });
   }
